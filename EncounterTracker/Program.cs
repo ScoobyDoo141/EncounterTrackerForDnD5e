1acd4d0f2cc769439d97422e566c58d83ca68e59
henry <email@email.com>
2022-03-21 07:15:57 +0000

    Add project files.

---
+++ EncounterTracker/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterTracker5e
+{
+    class Program
+    {
+        static List<string> newEnemy(List<string>enemies)
+        {
+            bool error;
+            string newName;
+            int newHP;
+            string output = "";
+            do
+            { 
+                Console.WriteLine("What should the creature be called?");
+                newName = Console.ReadLine();
+                error = true;
+                for (int c = 0; c < enemies.ToArray().Length; c++)
+                {
+                    if (enemies[c] == newName)
+                    {
+                        error = false;
+                    }
+                }
+            } while (error == false);
+            do
+            {
+                error = true;
+                Console.WriteLine("How much health does the creature have?");
+                error = int.TryParse(Console.ReadLine(), out newHP);
+                if (newHP >= 10000)
+                {
+                    error = false;
+                }             
+            } while (error == false);
+            for (int c = 4; c > newHP.ToString().Length; c--)
+            {
+                output += "0";
+            }
+            output += newHP + newName;
+            enemies.Add(output);
+            return enemies;           
+        }
+        static List<string> dealDamage(List<string> enemies)
+        {
+            string enemyHit;
+            int errorCount = 0;
+            int damageDealt;
+            bool error;
+            do
+            {
+                if (errorCount >= 1)
+                {
+                    Console.WriteLine("Please enter the name of an existing enemy");
+                }
+                errorCount = 0;
+                error = false;
+                Console.WriteLine("Who do you want to take damage?");
+                enemyHit = Console.ReadLine().ToLower();
+                for (int c = 0; c < enemies.ToArray().Length; c++)
+                {
+                    if (enemies[c].Substring(4).ToLower() == enemyHit)
+                    {
+                        enemyHit = "" + c;
+                        error = true;
+                    }
+                }
+            } while (errorCount >= 1);
+            do
+            {             
+                if (error = false)
+                {
+                    Console.WriteLine("Please enter an integer");
+                }
+                error = true;
+                Console.WriteLine("How much damage do they take?");
+                error = int.TryParse(Console.ReadLine(), out damageDealt);                                            
+            } while (error = false);
+            string output = "";
+            string enemyName = enemies[int.Parse(enemyHit)].Substring(4);
+            int enemyHP = int.Parse(enemies[int.Parse(enemyHit)].Substring(0, 4));
+            enemyHP -= damageDealt;
+            for (int c = 4; c > enemyHP.ToString().Length; c--)
+            {
+                output += "0";
+            }
+            output += enemyHP + enemyName;
+            enemies[int.Parse(enemyHit)] = output;
+            return (enemies);
+        }
+        static void Main(string[] args)
+        {
+            List<string> enemies = new List<string>();
+         
+            while (true)
+            {
+                Console.WriteLine("Do you want another creature? Y/N");
+                if (Console.ReadLine().ToUpper() == "Y")
+                {
+                    enemies = newEnemy(enemies);
+                }
+                Console.WriteLine("Do any creatures take damage? Y/N");
+                if (Console.ReadLine().ToUpper() == "Y")
+                {
+                    enemies = dealDamage(enemies);
+                }
+                for (int c = 0; c < enemies.ToArray().Length; c++)
+                {                  
+                    Console.WriteLine("{0} has {1} health", enemies[c].Substring(4), enemies[c].Substring(0,4));
+                }
+            }
+        }       
+    }
+}
+
+
